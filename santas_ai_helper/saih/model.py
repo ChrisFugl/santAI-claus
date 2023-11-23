1d049c0152a82f20b55fd00cffd90c44063276ea
Christoffer Koo Øhrstrøm <christoffer.ohrstrom@visma.com>
2023-11-23 15:12:46 +0000

    Log training and evaluation metric

--- santas_ai_helper/saih/model.py
+++ santas_ai_helper/saih/model.py
@@ -1,5 +1,6 @@
 import lightning.pytorch as pl
 import torch
+import torchmetrics
 from transformers import BertForSequenceClassification
 
 from saih.constants import LABEL_TO_CLASS
@@ -18,6 +19,11 @@ class Model(pl.LightningModule):
         self._model = BertForSequenceClassification.from_pretrained(
             MODEL_NAME, num_labels=len(LABEL_TO_CLASS),
         )
+
+        self._accuracy = torchmetrics.Accuracy()
+        self._precision = torchmetrics.Precision()
+        self._recall = torchmetrics.Recall()
+        self._f1 = torchmetrics.F1()
 
     def training_step(self, batch, batch_idx):
         inputs, labels = batch
@@ -39,6 +45,19 @@ class Model(pl.LightningModule):
             output_attentions=False,
         )
         self.log("val/loss", outputs.loss, on_step=False, on_epoch=True)
+
+        predictions = torch.argmax(outputs.logits, dim=1)
+
+        self._accuracy(predictions, labels)
+        self._precision(predictions, labels)
+        self._recall(predictions, labels)
+        self._f1(predictions, labels)
+
+        self.log("val/accuracy", self._accuracy, on_step=False, on_epoch=True)
+        self.log("val/precision", self._precision, on_step=False, on_epoch=True)
+        self.log("val/recall", self._recall, on_step=False, on_epoch=True)
+        self.log("val/f1", self._f1, on_step=False, on_epoch=True)
+
         return outputs.loss
 
     def test_step(self, batch, batch_idx):
@@ -50,6 +69,19 @@ class Model(pl.LightningModule):
             output_attentions=False,
         )
         self.log("test/loss", outputs.loss, on_step=False, on_epoch=True)
+
+        predictions = torch.argmax(outputs.logits, dim=1)
+
+        self._accuracy(predictions, labels)
+        self._precision(predictions, labels)
+        self._recall(predictions, labels)
+        self._f1(predictions, labels)
+
+        self.log("val/accuracy", self._accuracy, on_step=False, on_epoch=True)
+        self.log("val/precision", self._precision, on_step=False, on_epoch=True)
+        self.log("val/recall", self._recall, on_step=False, on_epoch=True)
+        self.log("val/f1", self._f1, on_step=False, on_epoch=True)
+
         return outputs.loss
 
     def configure_optimizers(self):
