336bfd1c81bc2db2336ea26632b54822a413c41f
Christoffer Koo Øhrstrøm <christoffer.ohrstrom@visma.com>
2023-11-27 19:43:34 +0000

    Use probits for ensemble

--- santas_ai_helper/saih/ensemble_model.py
+++ santas_ai_helper/saih/ensemble_model.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import lightning.pytorch as pl
 import torch
 import torchmetrics
@@ -6,12 +8,13 @@ from saih.model import Model
 
 
 class EnsembleModel(pl.LightningModule):
-    def __init__(self, model_dirs: list[str]):
+    def __init__(self, model_names: list[str]):
         super().__init__()
 
         self._models = torch.nn.ModuleList()
-        for model_dir in model_dirs:
-            model = Model.load_from_checkpoint(model_dir)
+        for model_name in model_names:
+            checkpoint_dir = Path("lightning_logs") / model_name / "checkpoints" / "best.ckpt"
+            model = Model.load_from_checkpoint(checkpoint_dir)
             self._models.append(model)
 
         self._accuracy = torchmetrics.Accuracy(task="binary")
@@ -33,20 +36,16 @@ class EnsembleModel(pl.LightningModule):
             )
             logits += outputs.logits
 
-        # TODO: perhaps average the logits instead of summing them?
-
-        self.log("loss", outputs.loss, on_step=False, on_epoch=True)
-
-        predictions = torch.argmax(outputs.logits, dim=1)
+        probits = torch.softmax(logits, dim=1)
+        predictions = torch.argmax(probits, dim=1)
 
         self._accuracy(predictions, labels)
         self._precision(predictions, labels)
         self._recall(predictions, labels)
         self._f1(predictions, labels)
 
+        self.log("loss", outputs.loss, on_step=False, on_epoch=True)
         self.log("accuracy", self._accuracy, on_step=False, on_epoch=True)
         self.log("precision", self._precision, on_step=False, on_epoch=True)
         self.log("recall", self._recall, on_step=False, on_epoch=True)
         self.log("f1", self._f1, on_step=False, on_epoch=True)
-
-        return outputs.loss
