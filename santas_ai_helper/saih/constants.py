159956348dfd06b6d9d58d38229deb36c9bf60b3
Christoffer Koo Øhrstrøm <christoffer.ohrstrom@visma.com>
2023-11-27 20:14:44 +0000

    Fix output dir level

--- santas_ai_helper/saih/constants.py
+++ santas_ai_helper/saih/constants.py
@@ -6,4 +6,4 @@ MODEL_NAME = "bert-base-uncased"
 LABEL_TO_CLASS = {"Naughty": 0, "Nice": 1}
 CLASS_TO_LABEL = {0: "Naughty", 1: "Nice"}
 
-MODELS_DIR = Path(__file__).parent / "models"
+MODELS_DIR = Path(__file__).parent.parent / "models"
